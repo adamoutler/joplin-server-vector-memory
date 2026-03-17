37e1e6f0518d5584853bb37d45cd72e2cb65c449
Adam Outler <adamoutler@gmail.com>
2026-03-16 21:09:37 -0400

    chore: remove root junk test scripts

--- echo_server.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI, Request
-app = FastAPI()
-@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
-async def echo(request: Request, path: str):
-    return {"path": request.url.path, "method": request.method}
--- test_proxy.js
+++
@@ -1,3 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const proxy = createProxyMiddleware({ pathFilter: '/http-api', target: 'http://localhost:8000' });
-console.log(proxy);
