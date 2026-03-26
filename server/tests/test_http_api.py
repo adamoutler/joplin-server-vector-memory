51bb8b1d3dfb031e7e083e99ce48d5dde069cf12
Adam Outler <adamoutler@gmail.com>
2026-03-26 17:26:29 -0400

    Fix Flake8 error E261 in test_http_api.py

--- server/tests/test_http_api.py
+++ server/tests/test_http_api.py
@@ -294,7 +294,7 @@ def test_stateless_mcp_endpoint(temp_config_and_db):
 
     def get_free_port():
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('0.0.0.0', 0)) # codeql[py/bind-socket-all-network-interfaces]
+        s.bind(('0.0.0.0', 0))  # codeql[py/bind-socket-all-network-interfaces]
         port = s.getsockname()[1]
         s.close()
         return port
